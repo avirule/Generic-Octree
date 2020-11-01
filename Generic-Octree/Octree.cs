06e8cc99a03301ac5a50fd71583604f9f2299187
Zavier Divelbiss <semiviral@gmail.com>
2020-11-01 07:11:12 -0600

    minor optimization, code cleanup

--- Generic-Octree/Octree.cs
+++ Generic-Octree/Octree.cs
@@ -3,15 +3,22 @@ using System.Runtime.CompilerServices;
 
 namespace Generic_Octree
 {
-    public class Octree<T> : INodeCollection<T>
+    public class Octree<T> : INodeCollection<T> where T : notnull
     {
         private readonly int _Extent;
         private readonly OctreeNode<T> _RootNode;
 
+        public T Value => _RootNode.Value;
+        public bool IsUniform => _RootNode.IsUniform;
+
+        public int EdgeLength { get; }
+
         public Octree(int edgeLength, T initialValue)
         {
             if ((edgeLength <= 0) || ((edgeLength & (edgeLength - 1)) != 0))
+            {
                 throw new ArgumentException($"Size must be a power of two ({edgeLength}).", nameof(edgeLength));
+            }
 
             _Extent = edgeLength >> 1;
             _RootNode = new OctreeNode<T>(initialValue);
@@ -19,12 +26,15 @@ namespace Generic_Octree
             EdgeLength = edgeLength;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T GetPoint(int x, int y, int z) => GetPointIterative(x, y, z);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private T GetPointIterative(int x, int y, int z)
         {
             OctreeNode<T> currentNode = _RootNode;
 
-            for (int extent = _Extent; !currentNode!.IsUniform; extent /= 2)
+            for (int extent = _Extent; !currentNode!.IsUniform; extent >>= 1)
             {
                 Octree.DetermineOctant(extent, ref x, ref y, ref z, out int octant);
 
@@ -34,16 +44,8 @@ namespace Generic_Octree
             return currentNode.Value;
         }
 
-        public int EdgeLength { get; }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public T GetPoint(int x, int y, int z) => GetPointIterative(x, y, z);
-
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void SetPoint(int x, int y, int z, T value) => _RootNode.SetPoint(_Extent, x, y, z, value);
-
-        public T Value => _RootNode.Value;
-        public bool IsUniform => _RootNode.IsUniform;
     }
 
     internal static class Octree
@@ -55,7 +57,7 @@ namespace Generic_Octree
         // top half quadrant indexes:
         // 5 7
         // 4 6
-        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void DetermineOctant(int extent, ref int x, ref int y, ref int z, out int octant)
         {
             octant = 0;
--- Generic-Octree/OctreeNode.cs
+++ Generic-Octree/OctreeNode.cs
@@ -2,12 +2,13 @@ using System.Runtime.CompilerServices;
 
 namespace Generic_Octree
 {
-    public class OctreeNode<TNode>
+    public class OctreeNode<TNode> where TNode : notnull
     {
         private OctreeNode<TNode>[]? _Nodes;
 
         public TNode Value { get; private set; }
         public bool IsUniform => _Nodes == null;
+
         public OctreeNode<TNode>? this[int index] => _Nodes?[index];
 
         /// <summary>
@@ -16,22 +17,15 @@ namespace Generic_Octree
         /// <param name="value">Initial value of the collection.</param>
         public OctreeNode(TNode value) => Value = value;
 
-        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-        public TNode GetPoint(int extent, int x, int y, int z)
-        {
-            if (IsUniform) return Value;
-
-            Octree.DetermineOctant(extent, ref x, ref y, ref z, out int octant);
-
-            return _Nodes![octant].GetPoint(extent >> 1, x, y, z);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void SetPoint(int extent, int x, int y, int z, TNode newValue)
         {
             if (IsUniform)
             {
-                if (Value.Equals(newValue)) return;
+                if (Value.Equals(newValue))
+                {
+                    return;
+                }
                 else if (extent < 1)
                 {
                     // reached smallest possible depth (usually 1x1x1) so
@@ -39,7 +33,10 @@ namespace Generic_Octree
                     Value = newValue;
                     return;
                 }
-                else Populate();
+                else
+                {
+                    Populate();
+                }
             }
 
             Octree.DetermineOctant(extent, ref x, ref y, ref z, out int octant);
@@ -49,7 +46,10 @@ namespace Generic_Octree
 
             // on each recursion back-step, ensure integrity of node
             // and collapse if all child node values are equal
-            if (CheckShouldCollapse()) Collapse();
+            if (CheckShouldCollapse())
+            {
+                Collapse();
+            }
         }
 
         private void Populate()
@@ -67,25 +67,33 @@ namespace Generic_Octree
             };
         }
 
-        private void Collapse()
-        {
-            Value = _Nodes![0].Value;
-            _Nodes = null;
-        }
-
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private bool CheckShouldCollapse()
         {
-            if (IsUniform) return false;
+            if (IsUniform)
+            {
+                return false;
+            }
 
             TNode firstValue = _Nodes![0].Value;
 
             // avoiding using linq here for performance sensitivity
             foreach (OctreeNode<TNode> octreeNode in _Nodes)
             {
-                if (!octreeNode.IsUniform || !octreeNode.Value.Equals(firstValue)) return false;
+                if (!octreeNode.IsUniform || !octreeNode.Value.Equals(firstValue))
+                {
+                    return false;
+                }
             }
 
             return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void Collapse()
+        {
+            Value = _Nodes![0].Value;
+            _Nodes = null;
         }
     }
 }
